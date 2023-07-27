98ad99c9ccb0eefd3616d0a2a18d4efb9e3b9abc
Alabhya Jindal <alabhya10@gmail.com>
2023-07-27 11:44:12 +0530

    Use pug template to render home page

--- server.js
+++ server.js
@@ -18,33 +18,14 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(express.static('public'))
 
+app.set('view engine', 'pug')
+
 const htmlFile = fs.readFileSync(`${__dirname}/index.html`, 'utf-8')
 let jsFile = fs.readFileSync(`${__dirname}/script.js`, 'utf-8')
 jsFile = `mapboxgl.accessToken = '${process.env.MAPBOX_TOKEN}'\n` + jsFile
 
 app.route('/').get((req, res) => {
-  res.status(200).send(`
-  <script src="https://accounts.google.com/gsi/client" async></script>
-  <div>
-  <p>Get a personal travel log</p>
-  <div id="g_id_onload"
-  data-client_id="259494879046-t6ejuqrlbmgdlqotrkes0fgev4iak86e.apps.googleusercontent.com"
-  data-context="signin"
-  data-ux_mode="popup"
-  data-login_uri="${process.env.URI}/redirect"
-  data-auto_prompt="false">
-</div>
-
-<div class="g_id_signin"
-  data-type="standard"
-  data-shape="rectangular"
-  data-theme="outline"
-  data-text="continue_with"
-  data-size="large"
-  data-logo_alignment="left">
-</div>
-</div>
-`)
+  res.render('home', { uri: process.env.URI })
 })
 
 async function verify(token) {
